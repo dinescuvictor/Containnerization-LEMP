2f8c6ca866b3ab0ae5e0d0b335d5b93adc6df890
dinescuvictor <dinescuvictor195@gmail.com>
2022-04-09 05:30:08 +0000

    initial

---
+++ compose-demo/src/mysql.php
@@ -0,0 +1,16 @@
+       <?php
+        $connection = new PDO('mysql:host=mysql;dbname=demo;charset=utf8', 'root', 'root');
+        $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'demo'");
+        $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
+
+        if (empty($tables)) {
+            echo '<p class="center">Nu exita table in baza de date <code>demo</code>.</p>';
+        } else {
+            echo '<p class="center">Baza de date demo conține urătoarele tabele:</p>';
+            echo '<ul class="center">';
+            foreach ($tables as $table) {
+                echo "<li>{$table}</li>";
+            }
+            echo '</ul>';
+        }
+        ?>
